af691ec77fcf5fcc41c969ad87e56a26dc220bf1
CohenDavis <davisd10@rpi.edu>
2022-04-17 02:58:31 -0400

    Added custom assert for easier debugging

--- verification.cu
+++ verification.cu
@@ -6,6 +6,21 @@
 #include "verification.hu"
 
 /**
+ * @brief Customized assert for this use case
+ *
+ * @param word1 word from one file
+ * @param word2 word from other file
+ * @param line line number being compared
+ */
+void custom_assert (std::string word1, std::string word2, unsigned int line)
+{
+  if (word1 != word2) {
+    std::string message = "ERROR: Input File and Decoded File do not match on line " + std::to_string(line);
+    std::cerr << message << std::endl;
+  }
+}
+
+/**
  * @brief Verifies that the decoded file is the same as the input file
  *
  * @param d_file decoded filename
@@ -36,6 +51,6 @@ void verify(const char *d_file, const char *i_file, unsigned int aNum)
     std::getline(decoded_file, dec_word);
     std::getline(input_file, ref_word);
 
-    assert(dec_word == ref_word);
+    custom_assert(dec_word, ref_word, i+1);
   }
 }
